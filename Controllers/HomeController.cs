cc53813011fbe7ca5f85e686f783bfa8a15421c7
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-09-01 14:45:47 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_11._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Welcome to the registration page.";
+
+            return View();
+        }
+
+        public ActionResult ThankYou(string firstname, string lastname)
+        {
+            ViewBag.Welcome = $"Welcome, {firstname} {lastname}. Thank you for registrating";
+            return View();
+        }
+
+    }
+}
